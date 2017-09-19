5568aba3f5fe3733e6d94ea774d7d181c2411324
rajesh <rajeshpeta67@gmail.com>
2017-09-19 08:11:53 +0530

    created maven project

---
+++ mavensampleproject/src/main/java/com/vls/maven/App.java
@@ -0,0 +1,13 @@
+package com.vls.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
